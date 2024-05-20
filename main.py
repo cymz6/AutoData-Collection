1779055877cdf287f63cdae681226f0881d36e78
川渝莽子 <105029472+cymz6@users.noreply.github.com>
2024-05-20 17:30:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -30,3 +30,6 @@ if __name__ == "__main__":
     fetch_and_save('http://xn--z7x900a.live/', 'data/肥猫.txt', 'okhttp/3.12.11')
     # 获取TVBox巧技接口
     fetch_and_save('http://cdn.qiaoji8.com/tvbox.json', 'data/巧技.txt', 'okhttp/3.12.11')
+    # 获取V2rayNG接口
+    fetch_and_save('https://raw.githubusercontent.com/peasoft/NoMoreWalls/master/list.txt', 'data/VPN.txt')
+    
