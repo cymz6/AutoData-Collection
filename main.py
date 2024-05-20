87fb2b0d55cfa92547360662660010ab60fb0303
川渝莽子 <105029472+cymz6@users.noreply.github.com>
2024-05-20 15:02:01 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import requests
+import os
 
 def fetch_and_save(url, save_path, user_agent=None):
+    # 确保保存路径的目录存在，如果不存在则创建
+    directory = os.path.dirname(save_path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     if user_agent:
         headers = {'User-Agent': user_agent}
         response = requests.get(url, headers=headers)
@@ -12,6 +18,6 @@ def fetch_and_save(url, save_path, user_agent=None):
 
 if __name__ == "__main__":
     # 获取lives.txt文件, IPTV电视直播文件
-    fetch_and_save('https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt', 'data/lives1.txt')
+    fetch_and_save('https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt', 'data/lives.txt')
     # 获取interface.json文件, tvbox文件
-    fetch_and_save('http://home.cymz.rr.nu/tvbox/interface.json', 'data/interface1.json', 'okhttp/3.12.11')
+    fetch_and_save('http://home.cymz.rr.nu/tvbox/interface.json', 'data/tvbox.json', 'okhttp/3.12.11')
