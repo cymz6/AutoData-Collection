40a37951e5572bbb30b94803e2fff841ec6530cb
川渝莽子 <105029472+cymz6@users.noreply.github.com>
2024-05-20 10:25:48 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import requests
+
+def fetch_and_save(url, save_path):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(save_path, 'wb') as f:
+        f.write(response.content)
+
+if __name__ == "__main__":
+    # 获取good.json
+    fetch_and_save('https://raw.githubusercontent.com/shidahuilang/shuyuan/shuyuan/good.json', 'data/good.json')
+    # 获取book.json
+    fetch_and_save('https://raw.githubusercontent.com/shidahuilang/shuyuan/shuyuan/book.json', 'data/book.json')
