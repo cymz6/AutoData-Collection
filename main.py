403d7a1e2699cb36542dadc25277d46ffdf4b1f7
川渝莽子 <105029472+cymz6@users.noreply.github.com>
2024-05-20 17:20:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -32,7 +32,5 @@ if __name__ == "__main__":
     fetch_and_save('http://cdn.qiaoji8.com/tvbox.json', 'data/巧技.txt', 'okhttp/3.12.11')
     # 获取TVBox饭太硬接口
     fetch_and_save('https://www.xn--sss604efuw.top/tv/', 'data/饭太硬.bmp', 'okhttp/3.12.11')
-    # 获取TVBox饭太硬接口
-    fetch_and_save('https://raw.githubusercontent.com/gaotianliuyun/gao/master/0827.json', 'data/FongMI.json', 'okhttp/3.12.11')
     
 
