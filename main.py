332e92de1bb9c3923682555cb2740602ef806865
川渝莽子 <105029472+cymz6@users.noreply.github.com>
2024-05-20 14:30:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ def fetch_and_save(url, save_path):
         f.write(response.content)
 
 if __name__ == "__main__":
-    # 获取lives.txt文件
+    # 获取lives.txt文件,IPTV电视直播文件
     fetch_and_save('https://raw.githubusercontent.com/ssili126/tv/main/itvlist.txt', 'data/lives.txt')
